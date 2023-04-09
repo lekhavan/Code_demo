dca26566d9a4916e1ac4c9addc08c5d1be1f865f
BaoGCC19240 <91667147+BaoGCC19240@users.noreply.github.com>
2023-04-09 14:19:52 +0700

    Update create_user.php

--- create_user.php
+++ create_user.php
@@ -23,47 +23,92 @@
 		<label for="department">Department:</label>
 		<select name="department" id="department" required>
 			<option value="">-- Select Department --</option>
-			<option value="1">Sales</option>
-			<option value="2">Marketing</option>
-			<option value="3">Engineering</option>
-			<option value="4">HR</option>
+			<?php
+			// Connect to database
+			include_once('connection.php');
+
+			// Get list of departments from database
+			$sql = "SELECT DepartmentID, DepartmentName FROM Departments ORDER BY DepartmentName";
+			$result = $conn->query($sql);
+
+			// Loop through departments and display in select element
+			if ($result->num_rows > 0) {
+				while($row = $result->fetch_assoc()) {
+					echo "<option value=\"" . $row["DepartmentID"] . "\">" . $row["DepartmentName"] . "</option>";
+				}
+			}
+			?>
+			<option value="new">Add New Department</option>
 		</select>
+
+		<div id="new-department">
+			<label for="new_department_name">New Department Name:</label>
+			<input type="text" name="new_department_name" id="new_department_name">
+		</div>
 
 		<label for="terms">Agree to Terms:</label>
 		<input type="checkbox" name="terms" id="terms" required>
 
-		<input type="submit" value="Create">
-	</form>
-
-	<?php
-    include_once('connection.php');
-		// Check if form submitted
-		if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-			// Get form data
-			$firstname = $_POST['firstname'];
-			$lastname = $_POST['lastname'];
-			$email = $_POST['email'];
-			$password = $_POST['password'];
-			$department = $_POST['department'];
-			$terms = isset($_POST['terms']) ? 1 : 0;
-
-            $password_hashed = password_hash($password, PASSWORD_DEFAULT);
-
-			// Prepare and bind statement
-			$stmt = $conn->prepare("INSERT INTO Users (FirstName, LastName, Email, Password, AgreedToTerms, DepartmentID) VALUES (?, ?, ?, ?, ?, ?)");
-			$stmt->bind_param("ssssii", $firstname, $lastname, $email, $password_hashed, $terms, $department);
-
-			// Execute statement
-			if ($stmt->execute()) {
-				echo "<p>User created successfully!</p>";
-			} else {
-				echo "<p>Error creating user: " . $conn->error . "</p>";
-			}
-
-			// Close statement and connection
-			$stmt->close();
-			$conn->close();
-		}
-	?>
+		<input type="submit" name="create" value="Create">
+		</form>
 </body>
-</html>
+<?php
+if(isset($_POST['create'])){
+include_once('connection.php');
+// Get user input
+$firstname = $_POST['firstname'];
+$lastname = $_POST['lastname'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+$department_id = $_POST['department'];
+$new_department_name = (isset($_POST['new_department_name'])) ? $_POST['new_department_name'] : '';
+
+
+// Hash password
+$hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+// Check if new department was selected
+if ($department_id === 'new' && !empty($new_department_name)) {
+  // Insert new department into database using prepared statement
+  $insert_department_sql = "INSERT INTO Departments (DepartmentName) VALUES (?)";
+  $stmt = $conn->prepare($insert_department_sql);
+  $stmt->bind_param("s", $new_department_name);
+  $stmt->execute();
+
+  // Get ID of new department
+  $department_id = $stmt->insert_id;
+}
+$AgreedToTerms=true;
+// Insert user into database using prepared statement
+$insert_user_sql = "INSERT INTO Users (FirstName, LastName, Email, Password, AgreedToTerms, DepartmentID) VALUES (?, ?, ?, ?, ?, ?)";
+$stmt = $conn->prepare($insert_user_sql);
+$stmt->bind_param("ssssii", $firstname, $lastname, $email, $hashed_password, $AgreedToTerms ,$department_id);
+$stmt->execute();
+
+// Close prepared statement
+$stmt->close();
+}
+// Connect to database
+
+?>
+
+<script>
+	// Get the department select element and the new department input element
+const departmentSelect = document.getElementById('department');
+const newDepartmentInput = document.getElementById('new-department');
+
+// Hide the new department input element
+newDepartmentInput.style.display = 'none';
+
+// Add event listener to the department select element
+departmentSelect.addEventListener('change', function() {
+  if (departmentSelect.value === 'new') {
+    // Show the new department input element
+    newDepartmentInput.style.display = 'block';
+  } else {
+    // Hide the new department input element
+    newDepartmentInput.style.display = 'none';
+  }
+});
+</script>
+		
